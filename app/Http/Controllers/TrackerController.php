dc362c9ffbf8bae57ef3ed220a29d605032280e1
Francisco Padilla Muñoz <padilla.fco@gmail.com>
2017-11-01 17:35:06 +0000

    Importing data from .csv
    -New Model Tracker with migration and controller.

---
+++ app/Http/Controllers/TrackerController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Tracker;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Maatwebsite\Excel\Facades\Excel;
+
+class TrackerController extends Controller
+{
+    public function import() 
+    {
+    
+    	DB::table('trackers')->truncate();
+
+        Excel::load('database/SLCTDB170720.csv', function($reader) {
+            foreach ($reader->get() as $tracker) {
+                Tracker::create([
+                    'day' =>$tracker->day,
+                    'time' =>$tracker->time,
+                    'agent' =>$tracker->agent,
+                    'speaker' =>$tracker->speaker,
+                    'segmentation' =>$tracker->s,
+                    'lead' =>$tracker->l,
+                    'call' =>$tracker->c,
+                    'tMinute' =>$tracker->t,
+                    'deal' =>$tracker->d,
+                    'iSegundos' =>$tracker->ts,
+                    'tAvg' =>$tracker->avg,
+                    'iSecondsAvg' =>$tracker->avgs
+                    ]);
+            }
+        });
+    }
+}
---
+++ app/Tracker.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+{
+    protected $fillable = ['day',
+                            'time',
+                            'agent',
+                            'speaker',
+                            'segmentation',
+                            'lead',
+                            'call',
+                            'tMinute',
+                            'deal',
+                            'iSegundos',
+                            'tAvg',
+                            'iSecondsAvg'
+                            ];
+}
